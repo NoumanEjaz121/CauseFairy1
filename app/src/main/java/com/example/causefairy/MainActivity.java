ad826eec5d525d818fcfdf90138e460da532161b
19427445 <65602555+19427445@users.noreply.github.com>
2020-08-31 13:20:57 +1000

    CF1 talking to milo
    sign up/ sign in

--- app/src/main/java/com/example/causefairy/MainActivity.java
+++ app/src/main/java/com/example/causefairy/MainActivity.java
@@ -1,30 +1,32 @@
 package com.example.causefairy;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    EditText etEmail, etPass;
-    Button btnLogin;
-    TextView tvReg, tvfrgtPass;
+    private EditText etEmail, etPass;
+    Button btnLogin, btnResendCode;
+    TextView tvReg, tvfrgtPass, tvVerifyMsg;
+
+    ProgressDialog progressDialog;
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +36,12 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         etEmail = findViewById(R.id.etEmail);
         etPass = findViewById(R.id.etPass);
-        btnLogin = findViewById(R.id.btnLogin);
         tvReg = findViewById(R.id.tvReg);
+        btnLogin = findViewById(R.id.btnLogin);
         tvfrgtPass = findViewById(R.id.tvfrgtPass);
+        progressDialog = new ProgressDialog(this);
+        btnResendCode = findViewById(R.id.btnResendCode);
+        tvVerifyMsg = findViewById(R.id.tvVerifyMsg);
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -47,15 +52,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-        tvfrgtPass.setOnClickListener(new View.OnClickListener() {
+       /* tvfrgtPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent f = new Intent(MainActivity.this, Reset_Password.class);
                 startActivity(f);
             }
         });
-
+*/
         btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+
+      /*  btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email, pass;
@@ -91,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Incorrect Username or Password!", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
+        }); */
 
         tvReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,6 +112,73 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
                 finish();
 
+            }
+        });
+        /*
+        tvfrgtPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText resetMail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password?");
+                passwordResetDialog.setMessage("Enter your email to receive the Reset Link");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String mail = resetMail.getText().toString();
+                        mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this, "Rest Link has been sent to your email", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Error! Link not sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+               /* passwordResetDialog.setPositiveButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                passwordResetDialog.create().show();
+            }
+        }); */
+    }
+    private void Login(){
+        String email = etEmail.getText().toString();
+        String password = etPass.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            etEmail.setError("Enter your Email");
+            return;
+        }
+        else if(TextUtils.isEmpty(password)) {
+            etPass.setError("Enter your Password");
+            return;
+        }
+        progressDialog.setMessage("PLease Wait...");
+        progressDialog.show();
+        progressDialog.setCanceledOnTouchOutside(false);
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Sign In Successfull!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, HomePage.class );
+                    startActivity(i);
+                    finish();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Sign In Failed!", Toast.LENGTH_SHORT).show();
+                }
+                progressDialog.dismiss();
             }
         });
     }
--- app/src/main/java/com/example/causefairy/SignUp.java
+++ app/src/main/java/com/example/causefairy/SignUp.java
@@ -1,8 +1,15 @@
 package com.example.causefairy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.DialogTitle;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -13,24 +20,47 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignUp extends AppCompatActivity {
-    EditText etEmail, etName1, etName2, etPass, etConPass;
-    Button btnSignUp, btnInd, btnBus;
-    TextView tvLogin;
+    private static final String TAG = "SignUp";
+
+    private EditText etEmail, etName1, etName2, etPass, etConPass;
+    private Button btnSignUp, btnInd, btnBus;   //            android:id="@+id/btnSignUp"
+    private TextView tvLogin;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        etEmail = findViewById(R.id.etEmail);
-        etPass = findViewById(R.id.etPass);
+        firebaseAuth = FirebaseAuth.getInstance();
+
         etName1 = findViewById(R.id.etName1);
         etName2 = findViewById(R.id.etName2);
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPass);
         etConPass = findViewById(R.id.etConPass);
         btnSignUp = findViewById(R.id.btnSignUp);
         tvLogin = findViewById(R.id.tvLogin);
         btnInd = findViewById(R.id.btnInd);
         btnBus = findViewById(R.id.btnBus);
+        progressDialog = new ProgressDialog(this);
 
         btnInd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,7 +85,7 @@ public class SignUp extends AppCompatActivity {
             }
        });
 
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
+       /* btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name1, email, name2, pass;
@@ -84,6 +114,98 @@ public class SignUp extends AppCompatActivity {
                     startActivity(l);
                 }
             }
+        }); */
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
         });
+    }
+    private void Register(){
+        final String name1 = etName1.getText().toString();
+        final String name2 = etName2.getText().toString();
+        final String email = etEmail.getText().toString();
+        String password = etPass.getText().toString();
+        String conpass = etConPass.getText().toString();
+        if(TextUtils.isEmpty(name1)){
+            etName1.setError("Enter your Firstname");
+            return;
+        }
+        else if(TextUtils.isEmpty(name2)){
+            etName2.setError("Enter your Lastname");
+            return;
+        }
+        else if(TextUtils.isEmpty(email)){
+            etEmail.setError("Enter your Email");
+            return;
+        }
+        else if(TextUtils.isEmpty(password)){
+            etPass.setError("Enter your Password");
+            return;
+        }
+        else if(TextUtils.isEmpty(conpass)){
+            etConPass.setError("Confirm your Password");
+            return;
+        }
+        else if(!password.equals(conpass)){
+            etPass.setError("Passwords don't match");
+            return;
+        }
+        else if(password.length()<6){
+            etPass.setError("Password must be at least 6 characters long");
+            return;
+        }
+        else if(!isValidEmail(email)){
+            etEmail.setError("Invalid Email");
+            return;
+        }
+        progressDialog.setMessage("PLease Wait...");
+        progressDialog.show();
+        progressDialog.setCanceledOnTouchOutside(false);
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    FirebaseUser fUser = firebaseAuth.getCurrentUser();
+                    fUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(SignUp.this, "Verification email has been sent", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "Error, email not sent " + e.getMessage());
+                        }
+                    });
+
+                    Toast.makeText(SignUp.this, "Successfully Registered!", Toast.LENGTH_SHORT).show();
+                    userId = firebaseAuth.getCurrentUser().getUid();
+                    DocumentReference documentReference = db.collection("Users").document("userId");
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Firstname", name1);
+                    user.put("Lastname", name2);
+                    user.put("Email", email);
+                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "User Account has been created for " + userId);
+                        }
+                    });
+                    Intent i = new Intent(SignUp.this, HomePage.class );
+                    startActivity(i);
+                    finish();
+                }
+                else{
+                    Toast.makeText(SignUp.this, "Registration Failed!", Toast.LENGTH_SHORT).show();
+                }
+                progressDialog.dismiss();
+            }
+        });
+    }
+    private Boolean isValidEmail(CharSequence target){
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 }
